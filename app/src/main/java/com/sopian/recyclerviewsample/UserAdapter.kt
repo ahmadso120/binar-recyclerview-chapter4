9fc2f0b52f07fc9776495d61b47d026b492d8a67
Ahmad Sopian <sopianahmad120@gmail.com>
2022-04-04 11:32:04 +0700

    Use view binding to replace findViewById

--- app/src/main/java/com/sopian/recyclerviewsample/UserAdapter.kt
+++ app/src/main/java/com/sopian/recyclerviewsample/UserAdapter.kt
@@ -6,32 +6,37 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.sopian.recyclerviewsample.databinding.ItemUserBinding
 
 class UserAdapter(
-    private val user: List<User>,
+    private val listUser: List<User>,
     private var onItemClick: (User) -> Unit
 ) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_user,parent,false
+        return UserViewHolder(
+            ItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         )
-        return UserViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        holder.nameTextView.text = user[position].name
-        holder.phoneTextView.text = user[position].phone.toString()
-        holder.root.setOnClickListener {
-            onItemClick(user[position])
+        with(holder) {
+            val data = listUser[position]
+
+            binding.nameTextView.text = data.name
+            binding.phoneNumberTextView.text = data.phone.toString()
+
+            binding.root.setOnClickListener {
+                onItemClick(data)
+            }
         }
     }
 
-    override fun getItemCount(): Int = user.size
+    override fun getItemCount(): Int = listUser.size
 
-    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val nameTextView: TextView = itemView.findViewById(R.id.name_text_view)
-        val phoneTextView: TextView = itemView.findViewById(R.id.phone_number_text_view)
-        val root: ConstraintLayout = itemView.findViewById(R.id.root)
-    }
+    class UserViewHolder(val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root)
 }
